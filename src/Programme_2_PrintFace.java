285c1317546fea1a34e88f54bccfc7176f8389cb
alexjendra <khushbue51@gmail.com>
2023-10-20 13:02:29 +0100

    homeworksubmit

---
+++ src/Programme_2_PrintFace.java
@@ -0,0 +1,13 @@
+/**
+ * 2. Write a java program to print a face.
+ */
+public class Programme_2_PrintFace {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '_' | ");
+        System.out.println(" +_____+");
+    }
+
+}
